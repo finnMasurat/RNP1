92bdcca38f826524ff4e21c0cad79320697908d4
Finn Masurat <finn.masurat@hotmail.de>
2016-11-13 17:42:18 +0100

    nach dem praktikum

--- src/Client.java
+++ src/Client.java
@@ -3,6 +3,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.nio.charset.Charset;
 
 
 public class Client {
@@ -10,23 +11,33 @@ public class Client {
     public static void main(String[] args) {
 
         try {
-            Socket clientSocket = new Socket("127.0.0.1",6432);
+            Socket clientSocket = new Socket("127.0.0.1", 6432);
+            clientSocket.setKeepAlive(true);
+            boolean living = true;
+            while (living) {
+                // Nachricht vom Benutzer an den Client
+                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
-            // Nachricht vom Benutzer an den Client
-            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                // Nachricht vom Server an den Client
+                BufferedReader serverInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            // Nachricht vom Server an den Client
-            BufferedReader serverInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                // Nachricht vom Client an den Server
+                DataOutputStream clientOutput = new DataOutputStream(clientSocket.getOutputStream());
 
-            // Nachricht vom Client an den Server
-            DataOutputStream clientOutput = new DataOutputStream(clientSocket.getOutputStream());
+                System.out.println("Bitte Eingabe tätigen: ");
+                String eingabe = userInput.readLine()  + '\n';
 
-            System.out.println("Bitte Eingabe tätigen: ");
-            String eingabe = userInput.readLine();
+                clientOutput.writeUTF(eingabe);
+                //clientOutput.writeBytes(eingabe);
+                if (eingabe.equals("bye")) {
 
-            clientOutput.writeBytes(eingabe + '\n');
-
-
+                    living = false;
+                    serverInput.close();
+                    userInput.close();
+                    //clientOutput.close();
+                    clientSocket.close();
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/Server.java
+++ src/Server.java
@@ -1,49 +1,118 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
+    private final int MAX_CLIENTS = 3;
+    private static ServerSocket welcomeSocket = null;
+    private static List<ServerHelper> listenerSockets;
 
-    public static void main(String[] args) {
+    public Server(int port) {
+    }
 
+    public void start() {
         try {
-            ServerSocket welcomeSocket = new ServerSocket(6432);
-            Socket connectionSocket = new Socket();
-            connectionSocket = welcomeSocket.accept();
-
-            System.out.println("Connected..");
-
-            // Nachricht vom Client an den Server
-            BufferedReader serverInput = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
-            // Nachricht vom Server an den Client
-            DataOutputStream clientOutput = new DataOutputStream(connectionSocket.getOutputStream());
-
-            String inputLine;
-
-            System.out.println("Read from Client: ");
-            while((inputLine = serverInput.readLine()) != null ) {
-                System.out.println(inputLine);
-
-                String[] inputArray = inputLine.split(" ");
-
-                switch (inputArray[0]) {
-                    case "BYE":
-                        System.out.println("OK BYE");
-                        connectionSocket.close();
-                        break;
-
-                }
-
-            }
-
-
-
+            welcomeSocket = new ServerSocket(6432, 100);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        boolean serverAlive = true;
+        boolean listenForClients = true;
+        while (serverAlive) {
+            if (listenerSockets.size() < MAX_CLIENTS) {
+                try {
+                    Socket clientS = welcomeSocket.accept();
+                    ServerHelper clientListener;
+                    clientListener = new ServerHelper(clientS);
+                    clientListener.start();
+                    listenerSockets.add(clientListener);
+
+                } catch (IOException e) {
+                    System.err.println("Could not create Socket to listen for client");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 6432;
+        listenerSockets = new ArrayList<ServerHelper>();
+        Server server = new Server(port);
+        server.start();
+    }
+
+    public void remove() {
+        System.out.println(listenerSockets.size());
+        for (int i = 0; i < listenerSockets.size(); i++) {
+            if (listenerSockets.get(i).alive == false) {
+                listenerSockets.remove(i);
+                System.out.println("I removed one client");
+                System.out.println("Clients left: " + listenerSockets.size());
+            }
+        }
+    }
+
+    public class ServerHelper extends Thread {
+        private Socket socketForClient = null;
+        private boolean alive = true;
+
+        public ServerHelper(Socket socket) {
+            super("ServerHelper");
+            socketForClient = socket;
+        }
+
+        public void run() {
+            try {
+                socketForClient.setKeepAlive(true);
+            } catch (SocketException e) {
+                System.err.println("Could not set to keep the socket alive");
+            }
+
+            //BufferedReader serverInput = null;
+            try {
+                //Nachricht vom Client an den Server
+                //serverInput = new BufferedReader(new InputStreamReader(socketForClient.getInputStream()));
+                DataInputStream dos = null;
+                dos = new DataInputStream(socketForClient.getInputStream());
+                // Nachricht vom Server an den Client
+                DataOutputStream clientOutput = new DataOutputStream(socketForClient.getOutputStream());
+                String inputLine;
+                System.out.println("Client connected to Server");
+                while (alive) {
+                    //inputLine = serverInput.readLine();
+                    try {
+                        inputLine = dos.readUTF();
+                        System.out.println(inputLine + inputLine.length());
+                        byte[] bytes = "Côte d'Ivoire".getBytes("UTF-8");
+                        if (inputLine.equals("bye" + '\n')) {
+                            System.out.println("bye OK");
+                            socketForClient.setKeepAlive(false);
+                            alive = false;
+                            remove();
+                            clientOutput.close();
+                            dos.close();
+                            socketForClient.close();
+                            break;
+                        }
+                    } catch (EOFException e) {
+                        System.err.println("Client disconnected itself");
+                        socketForClient.setKeepAlive(false);
+                        alive = false;
+                        remove();
+                        clientOutput.close();
+                        dos.close();
+                        socketForClient.close();
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
